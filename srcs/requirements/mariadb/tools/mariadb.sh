732ee8b5fc3381e600a52d352d92e0aa1877a3ec
jgarcia <jgarcia@jgarcia.42.fr>
2023-12-01 14:14:28 +0100

    shell script updated

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,23 +1,22 @@
 #!/bin/sh
 /etc/init.d/mariadb start
 mysql_secure_installation << __EOF
+Y
+Y
 
-Y
-Y
-1234
-1234
+
 Y
 Y
 Y
 Y
 __EOF
-
 mysql -e "CREATE DATABASE IF NOT EXISTS testdb;"
 mysql -e "CREATE USER IF NOT EXISTS test@localhost IDENTIFIED BY 't3stp4ss';"
 mysql -e "GRANT ALL PRIVILEGES ON testdb.* TO test@localhost IDENTIFIED BY 't3stp4ss';"
+mysql -e "FLUSH PRIVILEGES;"
 mysql -e "GRANT ALL PRIVILEGES ON *.* TO root@localhost;"
 mysql -e "ALTER USER root@localhost IDENTIFIED BY '1234';"
-mysql -e "FLUSH PRIVILEGES;"
+mysql -u root -p1234 -e "FLUSH PRIVILEGES;"
 mysqladmin -u root -p1234 shutdown
 #exec /usr/sbin/mysqld --user=mysql --console
 
