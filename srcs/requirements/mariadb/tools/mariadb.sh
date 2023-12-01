d50b8818220f03d52e7e5cfacb6470b3cd8fc43e
jgarcia <jgarcia@jgarcia.42.fr>
2023-12-01 13:25:16 +0100

    tryin to fix mariadb

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 /etc/init.d/mariadb start
-mysql_secure_installation << EOF
-Y
-Y
-
+mysql_secure_installation << __EOF
 
 Y
 Y
+1234
+1234
 Y
 Y
-EOF
+Y
+Y
+__EOF
 
 mysql -e "CREATE DATABASE IF NOT EXISTS testdb;"
-exec /usr/sbin/mysqld --user=mysql --console
+mysql -e "CREATE USER IF NOT EXISTS test@localhost IDENTIFIED BY 't3stp4ss';"
+mysql -e "GRANT ALL PRIVILEGES ON testdb.* TO test@localhost IDENTIFIED BY 't3stp4ss';"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO root@localhost;"
+mysql -e "ALTER USER root@localhost IDENTIFIED BY '1234';"
+mysql -e "FLUSH PRIVILEGES;"
+mysqladmin -u root -p1234 shutdown
+#exec /usr/sbin/mysqld --user=mysql --console
+
