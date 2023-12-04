8ad9ec3366b10c7ee434fc48892414ad05d87db8
jgarcia <jgarcia@jgarcia.42.fr>
2023-12-04 14:44:51 +0100

    improvement

--- Makefile
+++ Makefile
@@ -12,19 +12,18 @@ all:
 		mkdir -p ~/data/mariadb; \
 	fi
 	sudo hostsed add 127.0.0.1 jgarcia.42.fr
-#	docker-compose build -f ./srcs/docker-compose.yml
 	docker-compose -f ./srcs/docker-compose.yml up --build -d
 
 clean:
 	sudo docker-compose -f ./srcs/docker-compose.yml down --rmi all -v
 
 fclean: clean
-	sudo hostsed rm 127.0.0.1 jgarcia.42.fr
+	sudo hostsed del 127.0.0.1 jgarcia.42.fr
 	if [ -d "/home/jgarcia/data/wordpress" ]; then \
-	sudo rm -rf /home/jgarcia/data/wordpress ; \
+		sudo rm -rf /home/jgarcia/data/wordpress ; \
 	fi
 	if [ -d "/home/jgarcia/data/mariadb" ]; then \
-	sudo rm -rf /home/jgarcia/data/mariadb ; \
+		sudo rm -rf /home/jgarcia/data/mariadb ; \
 	fi
 
 re: fclean all
--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -6,20 +6,26 @@
 #                               |            
                                
 sleep 20
-rm -f /var/www/wordpress/wp-config.php
+if [ ! -f /var/www/wordpress/wp-config.php ]; then \
 wp-cli.phar config create --allow-root \
 	--dbname=testdb \
 	--dbuser=test \
 	--dbpass=t3stp4ss \
 	--dbhost=mariadb:3306 \
-	--path='/var/www/wordpress'
+	--path='/var/www/wordpress'; \
 wp-cli.phar core install --title=inception\
 	--admin_user=toto \
 	--admin_password=toto \
 	--admin_email=toto@42.fr \
 	--path='/var/www/wordpress' \
 	--url='https://jgarcia.42.fr' \
-	--allow-root
-
+	--allow-root ; \
+wp-cli.phar user create \
+	tata \
+	tata@42.fr \
+	--user_pass=tata \
+	--path=/var/www/wordpress \
+	--allow-root ; \
+fi
 
 php-fpm7.4 -F
